76dc156fe5109a57759d3f42204f28f2cfa5af45
Blasue1 <csh990000@gmail.com>
2025-08-12 15:43:59 +0900

    Delete csrc directory

--- csrc/uvm_dpi.d
+++
@@ -1,9 +0,0 @@
-uvm_dpi.o: /tools/Synopsys/vcs/V-2023.12-SP1/etc/uvm/dpi/uvm_dpi.cc \
- /tools/Synopsys/vcs/V-2023.12-SP1/etc/uvm/dpi/uvm_regex.cc \
- /tools/Synopsys/vcs/V-2023.12-SP1/include/vpi_user.h \
- /tools/Synopsys/vcs/V-2023.12-SP1/etc/uvm/dpi/uvm_hdl.c \
- /tools/Synopsys/vcs/V-2023.12-SP1/include/sv_vpi_user.h \
- /tools/Synopsys/vcs/V-2023.12-SP1/include/vpi_user.h \
- /tools/Synopsys/vcs/V-2023.12-SP1/include/veriuser.h \
- /tools/Synopsys/vcs/V-2023.12-SP1/include/svdpi.h \
- /tools/Synopsys/vcs/V-2023.12-SP1/etc/uvm/dpi/uvm_svcmd_dpi.c
