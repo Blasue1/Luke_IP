cb6a0c1d69de9a8f953fc5c2020b0cb2f8c5e933
Blasue1 <csh990000@gmail.com>
2025-08-12 15:52:18 +0900

    Delete run_test/csrc directory

--- run_test/csrc/uvm_verdi_dpi.d
+++
@@ -1,8 +0,0 @@
-uvm_verdi_dpi.o: \
- /tools/Synopsys/vcs/V-2023.12-SP1/etc/uvm/verdi/dpi/uvm_verdi_dpi.cpp \
- /tools/Synopsys/vcs/V-2023.12-SP1/include/vpi_user.h \
- /tools/Synopsys/vcs/V-2023.12-SP1/include/veriuser.h \
- /tools/Synopsys/vcs/V-2023.12-SP1/include/sv_vpi_user.h \
- /tools/Synopsys/vcs/V-2023.12-SP1/include/vpi_user.h \
- /tools/Synopsys/vcs/V-2023.12-SP1/include/vcs_vpi_user.h \
- /tools/Synopsys/vcs/V-2023.12-SP1/include/sv_vpi_user.h
